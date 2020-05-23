458d416fc5b6e28ee0e5de355f3c5195e6218835
Akhil Omar <akhilomar555@gmail.com>
2020-05-23 20:03:02 +0530

    force

---
+++ BasicCube3d/Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Reference to RigidBody component 
+    public Rigidbody rb;  
+
+    //Fixed update is used instead of Update because FixedUpdate is very suitable for physics like stuff
+    //velocity and force
+    void FixedUpdate()
+    {
+         rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
