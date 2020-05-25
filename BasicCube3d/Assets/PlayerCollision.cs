74a9a9f7a9b21533bd92b8be106b9f79a2adf906
Akhil Omar <akhilomar555@gmail.com>
2020-05-25 20:42:28 +0530

    Prefabs

---
+++ BasicCube3d/Assets/PlayerCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    void OnCollisionEnter(Collision collisionInfo){
+        if(collisionInfo.collider.tag == "AmitJi"){
+            Debug.Log("BikajiBhujia");
+            movement.enabled = false;
+        }
+    }
+}
