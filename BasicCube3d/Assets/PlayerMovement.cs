35f6b798b66f22f0eb12978af8c1036997f68630
Akhil Omar <akhilomar555@gmail.com>
2020-05-25 11:09:11 +0530

    cam position

--- BasicCube3d/Assets/PlayerMovement.cs
+++ BasicCube3d/Assets/PlayerMovement.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,11 +6,20 @@ public class PlayerMovement : MonoBehaviour
 {
     //Reference to RigidBody component 
     public Rigidbody rb;  
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
 
     //Fixed update is used instead of Update because FixedUpdate is very suitable for physics like stuff
     //velocity and force
     void FixedUpdate()
     {
-         rb.AddForce(0, 0, 2000 * Time.deltaTime);
+         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+         if(Input.GetKey("d")){
+             rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+         }
+         if(Input.GetKey("a")){
+             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+         }
     }
 }
+
